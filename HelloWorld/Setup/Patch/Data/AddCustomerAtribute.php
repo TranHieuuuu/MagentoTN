c7cb8de67b21a237a711ca222947517322dfa0b1
TranHieuuuu <92633433+TranHieuuuu@users.noreply.github.com>
2022-03-09 15:51:32 +0700

    Add files via upload

---
+++ HelloWorld/Setup/Patch/Data/AddCustomerAtribute.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Magenest\HelloWorld\Setup\Patch\Data;
+
+use Magento\Customer\Model\Customer;
+use Magento\Eav\Model\Config;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+class AddCustomerAtribute implements DataPatchInterface
+{
+    /**
+     * @var CustomerSetupFactory
+     */
+    private $customerSetupFactory;
+
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $setup;
+
+    /**
+     * @var Config
+     */
+    private $eavConfig;
+
+    /**
+     * AccountPurposeCustomerAttribute constructor.
+     * @param ModuleDataSetupInterface $setup
+     * @param Config $eavConfig
+     * @param CustomerSetupFactory $customerSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $setup,
+        Config $eavConfig,
+        CustomerSetupFactory $customerSetupFactory
+    )
+    {
+        $this->customerSetupFactory = $customerSetupFactory;
+        $this->setup = $setup;
+        $this->eavConfig = $eavConfig;
+    }
+
+    public function apply()
+    {
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $this->setup]);
+        $customerEntity = $customerSetup->getEavConfig()->getEntityType(Customer::ENTITY);
+        $attributeSetId = $customerSetup->getDefaultAttributeSetId($customerEntity->getEntityTypeId());
+        $attributeGroup = $customerSetup->getDefaultAttributeGroupId($customerEntity->getEntityTypeId(), $attributeSetId);
+        $customerSetup->addAttribute(Customer::ENTITY, 'use_data_patch', [
+            'type' => 'varchar',
+            'input' => 'text',
+            'label' => 'Use Data Patch',
+            'required' => true,
+            'default' => 0,
+            'visible' => true,
+            'user_defined' => true,
+            'system' => false,
+            'is_visible_in_grid' => true,
+            'is_used_in_grid' => true,
+            'is_filterable_in_grid' => true,
+            'is_searchable_in_grid' => true,
+            'position' => 300
+        ]);
+        $newAttribute = $this->eavConfig->getAttribute(Customer::ENTITY, 'use_data_patch');
+        $newAttribute->addData([
+            'used_in_forms' => ['adminhtml_checkout','adminhtml_customer','customer_account_edit','customer_account_create'],
+            'attribute_set_id' => $attributeSetId,
+            'attribute_group_id' => $attributeGroup
+        ]);
+
+        $eavSetup->addAttribute( Customer::ENTITY, 'use_data_patch', [
+                'type' => 'varchar',
+                'label' => 'Icon',
+                'input' => 'image',
+                'sort_order' => 333,
+                'source' => '',
+                'global' => ScopedAttributeInterface::SCOPE_STORE,
+                'visible' => true,
+                'required' => false,
+                'user_defined' => false,
+                'default' => null,
+                'group' => 'General Information',
+//                'backend' => 'Magenest\HelloWorld\Model\Category\Attribute\Backend\Image'
+            ]
+        );
+        $newAttribute->save();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    public static function getVersion()
+    {
+        return '1.0.1';
+    }
+}
