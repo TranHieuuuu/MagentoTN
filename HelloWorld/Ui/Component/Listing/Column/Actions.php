97b0f814ce7232b0b396cbe5b5fd65298387e8bb
TranHieuuuu <92633433+TranHieuuuu@users.noreply.github.com>
2022-03-09 15:52:29 +0700

    Add files via upload

---
+++ HelloWorld/Ui/Component/Listing/Column/Actions.php
@@ -0,0 +1,74 @@
+<?php
+declare(strict_types=1);
+
+namespace Magenest\HelloWorld\Ui\Component\Listing\Column;
+
+class Actions extends \Magento\Ui\Component\Listing\Columns\Column
+{
+
+    const URL_PATH_DELETE = 'magenest_helloworld/helloworld/delete';
+    const URL_PATH_DETAILS = 'magenest_helloworld/helloworld/details';
+    protected $urlBuilder;
+    const URL_PATH_EDIT = 'magenest_helloworld/helloworld/edit';
+
+    /**
+     * @param \Magento\Framework\View\Element\UiComponent\ContextInterface $context
+     * @param \Magento\Framework\View\Element\UiComponentFactory $uiComponentFactory
+     * @param \Magento\Framework\UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\UiComponent\ContextInterface $context,
+        \Magento\Framework\View\Element\UiComponentFactory $uiComponentFactory,
+        \Magento\Framework\UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+                if (isset($item['id_column'])) {
+                    $item[$this->getData('name')] = [
+                        'edit' => [
+                            'href' => $this->urlBuilder->getUrl(
+                                static::URL_PATH_EDIT,
+                                [
+                                    'id_column' => $item['id_column']
+                                ]
+                            ),
+                            'label' => __('Edit')
+                        ],
+                        'delete' => [
+                            'href' => $this->urlBuilder->getUrl(
+                                static::URL_PATH_DELETE,
+                                [
+                                    'id_column' => $item['id_column']
+                                ]
+                            ),
+                            'label' => __('Delete'),
+                            'confirm' => [
+                                'title' => __('Delete "${ $.$data.title }"'),
+                                'message' => __('Are you sure you wan\'t to delete a "${ $.$data.title }" record?')
+                            ]
+                        ]
+                    ];
+                }
+            }
+        }
+
+        return $dataSource;
+    }
+}
+
